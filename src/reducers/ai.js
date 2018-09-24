7c44cdee17a4db0d45ff89ec8f412b531f634a0a
Florian Lackner <florian.lackner@gmail.com>
2018-09-24 19:44:38 +0200

    store ai in state

--- src/reducers/ai.js
+++ src/reducers/ai.js
@@ -1,6 +1,7 @@
 import { ChessState } from '../chess_helpers'
 
 const initialState = {
+  ai: null,
   ai_is_white: null
 };
 
@@ -12,6 +13,10 @@ export const ai = (state = initialState, action) => {
           ai_is_white: action.ai_is_white
         });
       }
+    case 'AI_READY':
+      return Object.assign({}, state, {
+        ai: action.ai
+      });
     default:
       return state;
   }
