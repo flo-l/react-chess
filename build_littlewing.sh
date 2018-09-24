db11e43b60c3deab31fce0d7e8354170b3b58ea9
Florian Lackner <florian.lackner@gmail.com>
2018-09-24 16:47:14 +0200

    add littelwing as submodule

--- build_littlewing.sh
+++ build_littlewing.sh
@@ -1,3 +1,4 @@
+git submodule update --init --recursive
 cd vendor/littlewing
 cargo +nightly build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/release/littlewing.wasm --out-dir target
