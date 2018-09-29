f1905388cdaadbbc0a092ba7e6c4e260b1cd7e2c
Florian Lackner <florian.lackner@gmail.com>
2018-09-29 13:49:29 +0200

    hot reload the store too new littlewinf version

--- src/index.js
+++ src/index.js
@@ -10,16 +10,29 @@ import GameController from './containers/game_controller'
 
 import './index.css'
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  )
-);
+const configureStore = () => {
+  const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    )
+  );
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('./reducers', () => {
+      const nextRootReducer = require('./reducers/index');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
+};
+
 
 ReactDOM.render(
-  <Provider store={store}>
+  <Provider store={configureStore()}>
     <GameController />
   </Provider>,
   document.getElementById('root')
